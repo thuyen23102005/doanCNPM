660c8df4fb08c92829078c2c922895b4a1192a8c
thuyen23102005 <thuyendangminh11a221@gmail.com>
2025-07-08 23:09:33 +0700

    Add news

--- src/Component/LoginForm/LoginForm.jsx
+++ src/Component/LoginForm/LoginForm.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './LoginForm.css';
+import { FaUser, FaLock, FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const LoginForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
-    <div>
-      <h2>Đăng nhập</h2>
-      <form>
-        <input type="text" placeholder="Tên đăng nhập" />
-        <input type="password" placeholder="Mật khẩu" />
+    <div className='wrapper'>
+      <form action="">
+        <h1>Đăng nhập</h1>
+        <div className='input-box'>
+          <FaUser className='icon' />
+          <input type="text" placeholder="Tên đăng nhập" />
+
+        </div>
+        <div className='input-box'>
+          <FaLock className='icon' />
+          <input
+            type={showPassword ? "text" : "password"}
+            placeholder="Mật khẩu"
+          />
+
+          {
+            showPassword ? (
+              <FaEyeSlash
+                className='eye-icon active'
+                onClick={togglePassword}
+              />
+            ) : (
+              <FaEye
+                className='eye-icon'
+                onClick={togglePassword}
+              />
+            )
+          }
+        </div>
+        <div className='remember-forgot'>
+          <label> <input type="checkbox" />Ghi nhớ đăng nhập</label>
+          <a href='#'>Quên mật khẩu</a>
+        </div>
         <button type="submit">Đăng nhập</button>
+        <div className="register-link">
+          <p>Bạn chưa có tài khoản? <a href='#'>Đăng ký ngay.</a></p>
+        </div>
       </form>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Home from './Component/Home/Home';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <LoginForm />
   </React.StrictMode>
 );
 
