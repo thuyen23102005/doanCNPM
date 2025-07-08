4c3b03f2c347a8ed3190cc1a916de58a9ff85055
thuyen23102005 <thuyendangminh11a221@gmail.com>
2025-07-08 23:21:17 +0700

    aaa

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import LoginForm from './Component/LoginForm/LoginForm';
-import Home from './Component/Home/Home';
-import FoodAndDrink from './Component/Food&Drink/Food&Drink';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FoodAndDrink/>
+    <LoginForm />
   </React.StrictMode>
 );
 
